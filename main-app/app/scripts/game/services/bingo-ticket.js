034f282ba3f6c9dc12205b2d98420de4fd7b5229
Liam Armour <liamarmour1991@googlemail.com>
2015-11-03 14:26:01 +0000

    working on tests

---
+++ main-app/app/scripts/game/services/bingo-ticket.js
@@ -0,0 +1,4 @@
+/**
+ * Created by liam.armour on 03/11/15.
+ */
+//bingo ticket service will go here.
--- main-app/tests/mocks/mocks.js
+++ main-app/tests/mocks/mocks.js
@@ -2,9 +2,19 @@ var mocks;
 (function () {
     'use strict';
     mocks = {
+        LoginApi: {
+            createLoginData: function(usernameType, passwordType){},
+            callApiLogin: function(callName, requestData){},
+            loginButton: function (usernameType, passwordType) {}
+        },
         $state:{
             go: function () {},
             $current: 'playerSelection'
         }
     };
 })();
+
+
+
+
+
