9ea897fe46e562843fbd14fe392436c5de52be79
Liam Armour <liamarmour1991@googlemail.com>
2015-11-04 10:36:36 +0000

    Trying to work on the bingo ticket string

--- main-app/app/scripts/game/services/bingo-ticket.js
+++ main-app/app/scripts/game/services/bingo-ticket.js
@@ -1,4 +1,38 @@
-/**
- * Created by liam.armour on 03/11/15.
- */
-//bingo ticket service will go here.
+(function () {
+    'use strict';
+    angular.module('Tombola.Games.Bingo90.Core')
+
+        .service('BingoTicket', ['$scope', 'GameProxy',  function($scope, gameProxy) {
+            $scope.gameModel = gameProxy;
+
+            var bingoTicket = gameModel.returnedMessage.payload.card,
+                ticketLength = bingoTicket.length,
+                tickets = [],
+                allTickets = [],
+                ticketGrid = [];
+
+            $scope.bingoStrip = [];
+
+            var createBingoTicketGrid = function () {
+                me.ticketGrid = [
+                    [null, null,null,null,null,null,null,null,null],
+                    [null, null,null,null,null,null,null,null,null],
+                    [null, null,null,null,null,null,null,null,null]
+                ];
+            };
+
+            var generateTheTicket = function (ticket) {
+                var startOfString = 0;
+                var endOfString = 30;
+                for(startOfString; startOfString < ticket.length; startOfString += 30){
+                    tickets.push(ticket.slice(startOfString, endOfString));
+                    endOfString += 30;
+                }
+            };
+
+
+
+
+
+        }]);
+})();
