00332148d36325bef14c11904152cda9630f7664
Liam Armour <liamarmour1991@googlemail.com>
2015-11-05 11:28:31 +0000

    Each ticket splits up and the balls also split up

--- main-app/app/scripts/game/services/bingo-ticket.js
+++ main-app/app/scripts/game/services/bingo-ticket.js
@@ -3,33 +3,42 @@
     angular.module('Tombola.Games.Bingo90.Core')
         .service('BingoTicket', [ function() {
             var me = this;
+            var tickets = [],
+                balls = [];
 
             me.pushArray = function (card) {
-                console.log(card);
+                var cardss = "054963758028345266770611596982054963758028345266770611596982";
+                generateTheTicket(cardss);
+            };
 
-                };
+            var createBingoTicketGrid = function () {
+                me.ticketGrid = [
+                    [null, null,null,null,null,null,null,null,null],
+                    [null, null,null,null,null,null,null,null,null],
+                    [null, null,null,null,null,null,null,null,null]
+                ];
+            };
 
-            //ticketLength = bingoTicket.length,
-            //tickets = [];
+            var generateTheTicket = function (ticket) {
+                var startOfString = 0;
+                var endOfString = 30;
+                for(startOfString; startOfString < ticket.length; startOfString += 30){
+                    tickets.push(ticket.slice(startOfString, endOfString));
+                    endOfString += 30;
+                }
+                splitUpTheTicket(tickets[0]);
+                console.log(tickets);
+            };
 
-            //$scope.bingoStrip = [];
-
-            //var createBingoTicketGrid = function () {
-            //    me.ticketGrid = [
-            //        [null, null,null,null,null,null,null,null,null],
-            //        [null, null,null,null,null,null,null,null,null],
-            //        [null, null,null,null,null,null,null,null,null]
-            //    ];
-            //};
-
-            //var generateTheTicket = function (ticket) {
-            //    var startOfString = 0;
-            //    var endOfString = 30;
-            //    for(startOfString; startOfString < ticket.length; startOfString += 30){
-            //        tickets.push(ticket.slice(startOfString, endOfString));
-            //        endOfString += 30;
-            //    }
-            //};
+            var splitUpTheTicket = function (ticket) {
+                var startOfString = 0;
+                var endOfString = 2;
+                for(startOfString; startOfString < ticket.length; startOfString += 2){
+                    balls.push(ticket.slice(startOfString, endOfString));
+                    endOfString += 2;
+                }
+                console.log(balls);
+            };
 
         }]);
 })();
