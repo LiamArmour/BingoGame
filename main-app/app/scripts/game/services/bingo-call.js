cd58a667f1d2b934309534b47281c37625ce11a0
Liam Armour <liamarmour1991@googlemail.com>
2015-11-09 15:43:29 +0000

    Working on the ticket calls

--- main-app/app/scripts/game/services/bingo-call.js
+++ main-app/app/scripts/game/services/bingo-call.js
@@ -2,19 +2,24 @@
     'use strict';
     angular.module('Tombola.Games.Bingo90.Core')
         .service('BingoCall',  ['$interval','BingoTicket','GameProxy', function ($interval, bingoTicket, gameProxy) {
-            var me = this,
-                getCallData = {
+            var me = this;
+
+            me.lastCalls = [];
+
+            var getCallData = {
                     gameId: 1,
                     userId: "drwho",
                     balance: 100000000000,
-                    callnumber: 1
+                    callnumber: 0
                 },
 
             makeApiCall = function(apiName, action, data, token){
                 gameProxy.callApi(apiName, action, data, token)
                     .then(function (data) {
                         me.callMessage = data;
+                        me.lastCalls.push(data.payload.call);
                         console.log(data);
+                        console.log(me.lastCalls);
                     }).catch(function (data) {
                         /* Error stub */
                         console.log(data);
@@ -24,9 +29,9 @@
             me.getCall = function (token) {
                 $interval(function(){
                     makeApiCall("game/getcall", "POST", getCallData, token);
-                    //getCallData.callnumber += 1;
+                    getCallData.callnumber += 1;
                     console.log(getCallData.callnumber);
-                },5000, 1);
+                },5000);
 
             };
 
