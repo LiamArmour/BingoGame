b7126cf032ccf360c7be70f837f397784e31fd4a
Liam Armour <liamarmour1991@googlemail.com>
2015-11-10 09:56:49 +0000

    Working on end of game and wins

--- main-app/app/scripts/game/services/bingo-call.js
+++ main-app/app/scripts/game/services/bingo-call.js
@@ -4,14 +4,13 @@
         .service('BingoCall',  ['$interval','BingoTicket','GameProxy', function ($interval, bingoTicket, gameProxy) {
             var me = this;
 
-            me.lastCalls = [];
             me.lastCallsDisplay = [];
 
             var getCallData = {
                     gameId: 1,
                     userId: "drwho",
                     balance: 100000000000,
-                    callnumber: 0
+                    callnumber: 80
                 },
 
                 removeFirstElement = function () {
@@ -20,14 +19,28 @@
                     }
                 },
 
+                checkForLine = function (response) {
+                    if(response.message === "Line"){
+                        alert('Line Prize' + response.payload.winnerInfo.linewinnername + response.payload.winnerInfo.lineprize);
+                    }
+                },
+
+                checkForHouse = function (response) {
+                    if(response.message === "Winner"){
+                        alert('House Prize' + response.payload.winnerInfo.linewinnername + response.payload.winnerInfo.lineprize);
+                        $interval.cancel(me.getCall());
+                    }
+                },
+
             makeApiCall = function(apiName, action, data, token){
                 gameProxy.callApi(apiName, action, data, token)
                     .then(function (data) {
                         me.callMessage = data;
-                        me.lastCalls.push(data.payload.call);
                         me.lastCallsDisplay.push(data.payload.call);
                         console.log(data);
                         removeFirstElement();
+                        checkForLine(data);
+                        checkForHouse(data);
                     }).catch(function (data) {
                         /* Error stub */
                         console.log(data);
@@ -39,7 +52,7 @@
                     makeApiCall("game/getcall", "POST", getCallData, token);
                     getCallData.callnumber += 1;
                     console.log(getCallData.callnumber);
-                },5000);
+                },1000);
 
             };
 
