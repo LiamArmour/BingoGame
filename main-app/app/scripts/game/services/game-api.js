d71325859a83c3fdb42f7b338fcd8ccb2851a238
Liam Armour <liamarmour1991@googlemail.com>
2015-11-13 10:32:21 +0000

    Fixed

--- main-app/app/scripts/game/services/game-api.js
+++ main-app/app/scripts/game/services/game-api.js
@@ -6,29 +6,34 @@
                 buyInData = {
                     gameId: 1,
                     userId: sessionDetails.login.username,
-                    balance: 100000
+                    balance: sessionDetails.login.balance
                 },
 
                 callApi = function(apiName, action, data, token){
-                        gameProxy.callApi(apiName, action, data, token)
-                            .then(function (data) {
-                                me.returnedMessage = data;
-                                $state.go(data.message);
-                                if(data.message == "TicketBought"){
-                                    bingoTicket.pushArray(data.payload.card);
-                                    bingoCall.getCall(token);
-                                }
-                            }).catch(function (data) {
-                                /* Error stub */
-                                console.log(data);
-                            });
-                    };
+                    gameProxy.callApi(apiName, action, data, token)
+                        .then(function (data) {
+                            me.returnedMessage = data;
+                            $state.go(data.message);
+                            if(data.message == "TicketBought"){
+                                bingoTicket.pushArray(data.payload.card);
+                                bingoCall.getCall(token);
+                            }
+                        }).catch(function (data) {
+                            /* Error stub */
+                            console.log(data);
+                        });
+                };
 
             me.nextButton = function (token) {
                 callApi("game/next", "GET", "", token);
             };
 
             me.buyIn = function (token) {
+                buyInData = {
+                    gameId: 1,
+                    userId: sessionDetails.login.username,
+                    balance: sessionDetails.login.balance
+                };
                 callApi("game/buyticket", "POST", buyInData, token);
             };
 
