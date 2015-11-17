ce432d5270570290358d4cbdbda7385ffd2e484d
Liam Armour <liamarmour1991@googlemail.com>
2015-11-17 15:40:46 +0000

    Proxy constant test

---
+++ main-app/tests/core/services/proxy-constants.test.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+    describe('Testing the proxy constant', function () {
+        var constants;
+        beforeEach(function () {
+            module('Tombola.Games.Bingo90.Core');
+            inject(function ($injector) {
+                constants = $injector.get('ProxyConstants');
+            });
+        });
+
+        it('Ensures the string is equal to the correct url', function () {
+            constants.baseURL.should.equal('http://localhost:30069/');
+        });
+
+    });
+
+})();
