a6be41e3a852921b7a68fefb4c6f138ccee92a0f
Liam Armour <liamarmour1991@googlemail.com>
2015-11-18 09:50:54 +0000

    Formatted the file

--- main-app/app/scripts/game/services/game-api.js
+++ main-app/app/scripts/game/services/game-api.js
@@ -1,24 +1,25 @@
 (function () {
     'use strict';
     angular.module('Tombola.Games.Bingo90.Core')
-        .service('GameApi',  ['$state', 'GameProxy','BingoTicket','BingoCall','SessionDetails', function ($state, gameProxy, bingoTicket, bingoCall, sessionDetails) {
+        .service('GameApi',
+        ['$state', 'GameProxy', 'BingoTicket', 'BingoCall', 'SessionDetails', function ($state, gameProxy, bingoTicket,
+                                                                                        bingoCall, sessionDetails) {
             var me = this,
-
-            callApi = function(apiName, action, data){
-                gameProxy.callApi(apiName, action, data)
-                    .then(function (data) {
-                        me.returnedMessage = data;
-                        console.log(sessionDetails.login);
-                        $state.go(data.message);
-                        if(data.message == "TicketBought"){
-                            bingoTicket.pushArray(data.payload.card);
-                            bingoCall.getCall();
-                        }
-                    }).catch(function (data) {
-                        /* Error stub */
-                        console.log(data);
-                    });
-            };
+                callApi = function (apiName, action, data) {
+                    gameProxy.callApi(apiName, action, data)
+                        .then(function (data) {
+                            me.returnedMessage = data;
+                            console.log(sessionDetails.login);
+                            $state.go(data.message);
+                            if (data.message == "TicketBought") {
+                                bingoTicket.pushArray(data.payload.card);
+                                bingoCall.getCall();
+                            }
+                        }).catch(function (data) {
+                            /* Error stub */
+                            console.log(data);
+                        });
+                };
 
             me.nextButton = function () {
                 //Todo: expose getNextGame on game proxy
@@ -31,11 +32,11 @@
                     gameId: 1,
                     userId: sessionDetails.login.username,
                     balance: sessionDetails.login.balance
-                    };
+                };
                 console.log(buyInData);
                 console.log(sessionDetails.login.username);
                 console.log(sessionDetails.login.balance);
-                    callApi("game/buyticket", "POST", buyInData);
+                callApi("game/buyticket", "POST", buyInData);
             };
         }]);
 })();
