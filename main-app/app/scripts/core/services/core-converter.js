d8981e4901bb0a969e7de1d134a479bb05b4cbb7
Liam Armour <liamarmour1991@googlemail.com>
2015-11-18 17:20:54 +0000

    Little fixes

--- main-app/app/scripts/core/services/core-converter.js
+++ main-app/app/scripts/core/services/core-converter.js
@@ -48,7 +48,7 @@
                             }
                         }
                     };
-                    return ticketPurchaseData;
+                    me.ticketData = ticketPurchaseData;
                 };
 
             }]);
