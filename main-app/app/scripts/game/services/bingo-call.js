52055a595e13d8840e4d9ef4aa8708401c746129
Liam Armour <liamarmour1991@googlemail.com>
2015-11-18 10:39:08 +0000

    Fixing some code errors

--- main-app/app/scripts/game/services/bingo-call.js
+++ main-app/app/scripts/game/services/bingo-call.js
@@ -1,28 +1,30 @@
 (function () {
     'use strict';
     angular.module('Tombola.Games.Bingo90.Core')
-        .service('BingoCall',  ['$interval','$state','BingoTicket','GameProxy','EndOfGame','SessionDetails', function ($interval, $state, bingoTicket, gameProxy, endOfGame, sessionDetails) {
+        .service('BingoCall',
+        ['$interval', '$state', 'BingoTicket', 'GameProxy', 'EndOfGame', 'SessionDetails', function ($interval, $state,
+                                                                                                     bingoTicket,
+                                                                                                     gameProxy,
+                                                                                                     endOfGame,
+                                                                                                     sessionDetails) {
             var me = this,
-                sessionDetailsLogin = sessionDetails.login;
-            me.lastCallsDisplay = [];
-
-            var removeFirstElement = function () {
-                    if(me.lastCallsDisplay.length >= 6){
+                removeFirstElement = function () {
+                    if (me.lastCallsDisplay.length >= 6) {
                         me.lastCallsDisplay.splice(0, 1);
                     }
                 },
 
                 checkForNumber = function (callNumber) {
-                    if(callNumber.toString().length < 2){
-                        callNumber = "0"+callNumber.toString();
+                    if (callNumber.toString().length < 2) {
+                        callNumber = "0" + callNumber.toString();
                     }
 
-                    if(bingoTicket.balls.indexOf(callNumber.toString()) !== -1){
+                    if (bingoTicket.balls.indexOf(callNumber.toString()) !== -1) {
                         bingoTicket.markedNumbers[bingoTicket.balls.indexOf(callNumber.toString())] = "markedCell";
                     }
                 },
 
-                makeApiCall = function(apiName, action, data){
+                makeApiCall = function (apiName, action, data) {
                     gameProxy.callApi(apiName, action, data)
                         .then(function (data) {
                             me.callMessage = data;
@@ -36,6 +38,8 @@
                         });
                 };
 
+            me.lastCallsDisplay = [];
+
             me.getCall = function () {
                 var getCallData = {
                     gameId: 1,
@@ -43,10 +47,10 @@
                     balance: sessionDetails.login.balance,
                     callnumber: 0
                 };
-                me.gameLoop = $interval(function(){
+                me.gameLoop = $interval(function () {
                     makeApiCall("game/getcall", "POST", getCallData);
                     getCallData.callnumber += 1;
-                },1000, 90);
+                }, 1000, 90);
 
             };
 
