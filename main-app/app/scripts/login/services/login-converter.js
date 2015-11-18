55b990e3a4dd000d0bbcf3e633bd8e14a98d412c
Liam Armour <liamarmour1991@googlemail.com>
2015-11-18 16:19:37 +0000

    Adding more to the login convertor of the rare API data

--- main-app/app/scripts/login/services/login-converter.js
+++ main-app/app/scripts/login/services/login-converter.js
@@ -1,11 +1,17 @@
 (function () {
     'use strict';
     angular.module('Tombola.Games.Bingo90.Core')
-        .service('BingoCall',
-        [
+        .service('LoginConverter', [
             function () {
                 var me = this;
 
+                me.convertLoginData = function (response) {
+                    var userData = {
+                        username: response.username,
+                        balance: response.balance,
+                        token: response.token
+                    }
+                };
 
             }]);
 })();
