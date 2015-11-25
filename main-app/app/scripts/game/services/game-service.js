b429c6346635cfb9f1ad9b4b90f2d5f400e80e9e
Liam Armour <liamarmour1991@googlemail.com>
2015-11-25 13:11:53 +0000

    Updated more things

--- main-app/app/scripts/game/services/game-service.js
+++ main-app/app/scripts/game/services/game-service.js
@@ -1,3 +1,35 @@
-/**
- * Created by liam.armour on 25/11/15.
- */
+(function () {
+    'use strict';
+    angular.module('Tombola.Games.Bingo90.Core')
+        .service('GameService',
+        ['$state', 'GameProxy', 'BingoTicket', 'BingoCall', 'CoreApiConverter', function ($state, gameProxy,
+                                                                                          bingoTicket,
+                                                                                          bingoCall, coreApiConverter) {
+            var me = this;
+
+            me.nextGame = function (endPoint, type, data) {
+                gameProxy.callApi(endPoint, type, data)
+                    .then(function (data) {
+                        coreApiConverter.convertNextGameData(data);
+                        $state.go("NextGame");
+                    }).catch(function (data) {
+                        /* Error stub */
+                        console.log(data);
+                    });
+            };
+
+            me.buyInGame = function (endPoint, type, data) {
+                gameProxy.callApi(endPoint, type, data)
+                    .then(function (data) {
+                        coreApiConverter.convertTicketPurchaseData(data);
+                        $state.go("TicketBought");
+                        bingoTicket.pushArray(data.payload.card);
+                        bingoCall.getCall();
+                    }).catch(function (data) {
+                        /* Error stub */
+                        console.log(data);
+                    });
+            };
+
+        }]);
+})();
