b1a1cb8b78c96a5a68fe14c1610fa01c57a2673f
Liam Armour <liamarmour1991@googlemail.com>
2015-11-30 10:08:03 +0000

    Game proxy test are finished, not sure if these are correct.

--- main-app/tests/game/services/game-proxy.test.js
+++ main-app/tests/game/services/game-proxy.test.js
@@ -70,6 +70,94 @@
             httpBackend.flush();
         });
 
+        it('Ensures the get call works perfectly', function () {
+            var theResponse = {
+                "message": "Call",
+                "payload": {
+                    "gameId": 1,
+                    "callnumber": 1,
+                    "call": 54,
+                    "user": {
+                        "username": "drwho",
+                        "balance": 19990,
+                        "token": "f36bb73b-83cc-4539-aac0-893914bc73ec"
+                    }
+                }
+            };
+
+            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:30069/game/getcall", "")
+                .respond(theResponse);
+            var returnedPromise = gameProxy.callApi("game/getcall", "POST", "");
+            var result;
+            returnedPromise.then(function (response) {
+                result = response;
+                result.should.be.deep.equal(theResponse);
+            });
+            httpBackend.flush();
+        });
+
+        it('Ensures the get call LINE works', function () {
+            var theResponse = {
+                "message": "Line",
+                "payload": {
+                    "gameId": 1,
+                    "callnumber": 85,
+                    "call": 5,
+                    "user": {
+                        "username": "drwho",
+                        "balance": 19991,
+                        "token": "f36bb73b-83cc-4539-aac0-893914bc73ec"
+                    },
+                    "winnerInfo": {
+                        "linewinnername": "drwho",
+                        "lineprize": 1
+                    }
+                }
+            };
+
+            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:30069/game/getcall", "")
+                .respond(theResponse);
+            var returnedPromise = gameProxy.callApi("game/getcall", "POST", "");
+            var result;
+            returnedPromise.then(function (response) {
+                result = response;
+                result.should.be.deep.equal(theResponse);
+            });
+            httpBackend.flush();
+        });
+
+        it('Ensures the get call HOUSE works', function () {
+            var theResponse = {
+                "message": "Winner",
+                "payload": {
+                    "gameId": 1,
+                    "callnumber": 89,
+                    "call": 6,
+                    "user": {
+                        "username": "drwho",
+                        "balance": 19995,
+                        "token": "f36bb73b-83cc-4539-aac0-893914bc73ec"
+                    },
+                    "winnerInfo": {
+                        "linewinnername": "drwho",
+                        "lineprize": 1,
+                        "housewinnername": "drwho",
+                        "houseprize": 5
+                    }
+                }
+            };
+
+            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:30069/game/getcall", "")
+                .respond(theResponse);
+            var returnedPromise = gameProxy.callApi("game/getcall", "POST", "");
+            var result;
+            returnedPromise.then(function (response) {
+                result = response;
+                result.should.be.deep.equal(theResponse);
+            });
+            httpBackend.flush();
+        });
+
         afterEach(function () {
             httpBackend.verifyNoOutstandingExpectation();
             httpBackend.verifyNoOutstandingRequest();
